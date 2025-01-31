7d0a3fdbe230dbc1783327ebc2ac904ae7a64c14
AnshKumar299 <142929436+AnshKumar299@users.noreply.github.com>
2025-01-31 08:14:52 +0530

    Create Q14.java

---
+++ Assignment 1/Q14.java
@@ -0,0 +1,108 @@
+interface Shape
+{
+    void area();
+    void draw();
+    void rotate();
+    void move();
+}
+
+class Circle implements Shape
+{
+    float radius;
+    int n;
+    Circle(float r,int n)
+	{
+        radius=r;
+        this.n=n;
+	}
+
+	public void area()
+    {
+        System.out.println("Area of circle "+n+": "+ (3.14*radius*radius));
+    }    
+
+    public void draw()
+    {
+        System.out.println("Drawing a circle");
+
+    }
+
+    public void rotate()
+    {
+        System.out.println("Rotating the circle");
+    }
+
+    public void move()
+    {
+        System.out.println("Moving the circle");
+    }
+
+	void getDetails()
+	{
+		area();
+		draw();
+		rotate();
+		move();
+	}
+}
+
+class Rectangle implements Shape
+{       
+    float len,bre;
+    int n;
+	Rectangle(float l, float b,int n)
+	{
+		len=l;
+        bre=b;
+        this.n=n;
+	}
+
+    public void area()
+    {
+        System.out.println("Area of rectangle "+n+" : "+ (len*bre));
+    }
+
+    public void draw()
+    {
+        System.out.println("Drawing a rectangle");
+    }
+
+    public void rotate()
+    {
+        System.out.println("Rotating the rectangle");
+    }
+
+    public void move()
+    {
+        System.out.println("Moving the rectangle");
+    }
+
+	void getDetails()
+	{
+		area();
+		draw();
+		rotate();
+		move();
+	}
+}
+
+class Q14
+{
+	public static void main(String args[])
+	{
+		Circle c1 = new Circle(5f,1);
+		c1.getDetails();
+
+		Circle c2 = new Circle(7.6f,2);
+		c2.getDetails();
+
+		Rectangle r1 = new Rectangle(3f,4f,1);
+		r1.getDetails();
+
+		Rectangle r2 = new Rectangle(2.3f,3.1f,2);
+		r2.getDetails();
+
+		Rectangle r3 = new Rectangle(1f,2f,3);
+		r3.getDetails();
+	}
+}
